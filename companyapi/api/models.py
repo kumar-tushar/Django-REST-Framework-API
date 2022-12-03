c9d1d4e74e748331f35112dee25d1f57caa7de8e
Tushar Kumar Verma <tushar.workplace@gmail.com>
2022-12-03 18:59:34 +0530

    .

--- companyapi/api/models.py
+++ companyapi/api/models.py
@@ -6,6 +6,6 @@ class Company(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
     about = models.TextField()
-    type = models.CharField(max_length=100, choices=(('IT', 'IT'), ('Non-IT', 'Non-IT')))
+    type = models.CharField(max_length=100, choices=(('IT', 'IT'), ('Non IT', 'Non IT')))
     added_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
--- companyapi/api/views.py
+++ companyapi/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Company
+from .serializers import CompanySerializer
 
-# Create your views here.
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+
+
+
