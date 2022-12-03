7f37ed31010ffa611102cb6384e78a1ffc0c1124
Tushar Kumar Verma <tushar.workplace@gmail.com>
2022-12-03 12:00:43 +0530

    44:00

--- companyapi/api/models.py
+++ companyapi/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    about = models.TextField()
+    type = models.CharField(max_length=100, choices=(('IT', 'IT'), ('Non-IT', 'Non-IT')))
+    added_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
