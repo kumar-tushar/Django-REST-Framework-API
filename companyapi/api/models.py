563f8af74b2dba5bd915247774bc432a9771dbcc
Tushar Kumar Verma <tushar.workplace@gmail.com>
2022-12-03 19:31:37 +0530

    .

--- companyapi/api/models.py
+++ companyapi/api/models.py
@@ -9,3 +9,18 @@ class Company(models.Model):
     type = models.CharField(max_length=100, choices=(('IT', 'IT'), ('Non IT', 'Non IT')))
     added_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=10)
+    about = models.TextField()
+    position = models.CharField(max_length=50,
+                                choices=(('Manager', 'Manager'), ('SDE', 'SDE'), ('Project Leader', 'PL')))
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
