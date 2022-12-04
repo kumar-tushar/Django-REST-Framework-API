6bc09c49ef577e059987a5ebaf7b7ee11db06a42
Tushar Kumar Verma <tushar.workplace@gmail.com>
2022-12-04 14:30:10 +0530

    .

--- companyapi/api/views.py
+++ companyapi/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Company, Employee
 from .serializers import CompanySerializer, EmployeeSerializer
 
@@ -6,6 +8,13 @@ from .serializers import CompanySerializer, EmployeeSerializer
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+    @action(detail=True, methods=['get'])
+    def employees(self, request, pk=None):
+        company = Company.objects.get(pk=pk)
+        emps = Employee.objects.filter(company=company)
+        emps_serializer = EmployeeSerializer(emps, many=True, context={'request': request})
+        return Response(emps_serializer.data)
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
